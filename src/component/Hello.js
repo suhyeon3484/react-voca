99607cb3718cf3d5cd64bb14c5d678d25bcafd6d
suhyeon <suhyeon3484@naver.com>
2022-09-14 01:32:34 +0900

    #7 : useState

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,42 +1,22 @@
-// 모두 같은 표현
-// const Hello = function(){
-//     <p>Hello</p>;
-// }
-
-// const Hello = () => {
-//     <p>Hello</p>;
-// }
-
-import World from "./Word";
-import styles from "./Hello.module.css";
+// useState 사용할거면 꼭 import 해야 함
+import { useState } from "react";
 
 export default function Hello() {
-    function showNmae() {
-        console.log('Mike');
-    }
-    function showAge(age) {
-        console.log(age);
-    }
-    function showText(e){
-        console.log(e.target.value);
-    }
+    // let name = "Mike";
+    // 배열구조 사용 [state 명(변수명), state 변환해주는 함수], useState(초기값)
+    const [name, setName] = useState('Mike');
     
+    function changeName() {
+        // mike이면 jane으로 jane이면 mike로 바꿔줌
+        const newName = name === "Mike" ? "Jane" : "Mike";
+        // document.getElementById("name").innerText = name;
+        setName(newName);
+    }
+
     return (
     <div>
-    <h1>
-        Hello
-    </h1>
-    {/* 방법 2가지 */}
-    <button onClick={showNmae}>Show name</button>
-
-    {/*  매개변수 전달하기 편함 */}
-    <button 
-    onClick={() => {
-        showAge(10);
-    }}>
-        Show age
-    </button><br></br>
-    <input type='text' onChange={showText}></input>
+    <h2 id='name'>{name}</h2>
+    <button onClick={changeName}>change</button>
     </div>
     );
 }
