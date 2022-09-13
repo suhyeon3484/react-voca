c5303e43a0d2598c40e1b418dccb43b948664ee4
suhyeon <suhyeon3484@naver.com>
2022-09-14 02:49:20 +0900

    #9 더미 데이터 생성과 daylist 작성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Header from "./component/Header";
+import DayList from "./component/Daylist";
+
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <DayList />
     </div>
   );
 }
