15c340278c817c61e0d09771ecb8938ac781e5a7
suhyeon <suhyeon3484@naver.com>
2022-09-14 02:27:36 +0900

    #9 : 초기세팅

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import './App.css';
-import Hello from "./component/Hello";
-import Welcome from './component/Welcome';
-import styles from "./App.module.css";
-
 function App() {
   return (
     <div className="App">
-      <h3>props : properties</h3>
-      <Hello age={10}/>
-      <Hello age={20}/>
-      <Hello age={30}/>
+
     </div>
   );
 }
