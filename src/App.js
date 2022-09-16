25f0333e722e544517aecf0bb93d61c0c76dc161
suhyeon <suhyeon3484@naver.com>
2022-09-16 17:37:17 +0900

    끝

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Header from "./component/Header";
 import DayList from "./component/Daylist";
 import Day from "./component/Day";
+import { BrowserRouter, Route, Routes} from "react-router-dom"; 
 
 function App() {
   return (
+    // 스위치 외부는 모든 페이지에 공통으로 노출됨
+    <BrowserRouter>
     <div className="App">
       <Header />
-      <DayList />
-      <Day />
+      {/* 스위치 내부는 각각 url에 따라 다른 페이지를 보여즘 */}
+      <Routes>
+        <Route path="/">
+          <DayList />
+        </Route>
+        <Route path="/day">
+          <Day />
+        </Route>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
